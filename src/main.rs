f76f5f9b7f1c6f4971140081e500bc24c4c289d6
SnowCheetos <cassfunen@yahoo.com>
2023-06-23 17:07:45 -0500

    major problems...

--- src/main.rs
+++ src/main.rs
@@ -60,61 +60,66 @@ fn main() {
                 match line.as_str() {
                     "start" => {
                         println!("Starting server...");
-
                         *server_op_lock = Some(thread::spawn(move || {
-                            // Replace this with the actual function to start the server
-                            // REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT)
-                            if let Ok((redis_pid, gunicorn_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) {//&redis_conf_path, &workers, &address, &port, &timeout) {
+                            if let Ok((r_pid, g_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) {
                                 println!(
                                     "Server started with Redis PID: {}, Gunicorn PID: {}",
-                                    redis_pid.unwrap_or(0),
-                                    gunicorn_pid.unwrap_or(0)
+                                    r_pid.unwrap_or(0),
+                                    g_pid.unwrap_or(0)
                                 );
+                                // Assign the new PIDs to redis_pid and gunicorn_pid
+                                redis_pid = r_pid;
+                                gunicorn_pid = g_pid;
                             } else {
                                 println!("Error starting the server.");
                             }
                         }));
                     },
+                    
                     "stop" => {
                         println!("Stopping server...");
-
-                        // Capture the values of redis_pid and gunicorn_pid using the move keyword
-                        let redis_pid = redis_pid.expect("redis_pid not available");
-                        let gunicorn_pid = gunicorn_pid.expect("gunicorn_pid not available");
-                        *server_op_lock = Some(thread::spawn(move || {
-                            if let (Some(redis_pid), Some(gunicorn_pid)) = (redis_pid, gunicorn_pid) {
-                                server::stop_server(Some(redis_pid), Some(gunicorn_pid));
+                    
+                        if redis_pid.is_some() && gunicorn_pid.is_some() {
+                            *server_op_lock = Some(thread::spawn(move || {
+                                server::stop_server(redis_pid, gunicorn_pid);
                                 println!("Server stopped.");
-                            } else {
-                                println!("Server is not running.");
-                            }
-                        }));
+                            }));
+                        } else {
+                            println!("Redis PID or Gunicorn PID is not available.");
+                        }
                     },
+                    
                     "restart" => {
                         println!("Restarting server...");
-                        let redis_pid = redis_pid.expect("redis_pid not available");
-                        let gunicorn_pid = gunicorn_pid.expect("gunicorn_pid not available");
-                        // Restart the server in a new thread
-                        *server_op_lock = Some(thread::spawn(move || {
-                            if let (Some(redis_pid), Some(gunicorn_pid)) = (redis_pid, gunicorn_pid) {
-                                server::stop_server(Some(redis_pid), Some(gunicorn_pid));
+                        println!("Stopping server...");
+                    
+                        if redis_pid.is_some() && gunicorn_pid.is_some() {
+                            *server_op_lock = Some(thread::spawn(move || {
+                                server::stop_server(redis_pid, gunicorn_pid);
                                 println!("Server stopped.");
-                            } else {
-                                println!("Server is not running.");
-                            }
-                            println!("Server stopped.");
+                            }));
+                        } else {
+                            println!("Redis PID or Gunicorn PID is not available.");
+                        }
 
+                        "start" => {
                             println!("Starting server...");
-                            if let Ok((redis_pid, gunicorn_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) { // &redis_conf_path, &workers, &address, &port, &timeout) {
-                                println!(
-                                    "Server started with Redis PID: {}, Gunicorn PID: {}",
-                                    redis_pid.unwrap_or(0),
-                                    gunicorn_pid.unwrap_or(0)
-                                );
-                            } else {
-                                println!("Error starting the server.");
-                            }
-                        }));
+                            *server_op_lock = Some(thread::spawn(move || {
+                                if let Ok((r_pid, g_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) {
+                                    println!(
+                                        "Server started with Redis PID: {}, Gunicorn PID: {}",
+                                        r_pid.unwrap_or(0),
+                                        g_pid.unwrap_or(0)
+                                    );
+                                    // Assign the new PIDs to redis_pid and gunicorn_pid
+                                    redis_pid = r_pid;
+                                    gunicorn_pid = g_pid;
+                                } else {
+                                    println!("Error starting the server.");
+                                }
+                            }));
+                        },                        
+
                     },
                     "update" => {
                         println!("Updating server...");
