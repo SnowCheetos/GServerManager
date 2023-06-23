be63cd69ab3ee82486eb835aed0bd14cd6f9a4df
SnowCheetos <cassfunen@yahoo.com>
2023-06-23 16:55:49 -0500

    problem stopping

--- src/main.rs
+++ src/main.rs
@@ -18,39 +18,38 @@ const PORT: &str = "56009";
 const ADDRESS: &str = "0.0.0.0";
 
 fn main() {
-
     let scripts_dir = env::current_dir()
         .expect("Failed to retrieve current working directory")
         .join("scripts");
 
-    let first_folder = fs::read_dir(&scripts_dir)
-        .expect("Failed to read scripts directory")
-        .filter_map(Result::ok)
-        .find(|entry| entry.file_type().ok().map(|ft| ft.is_dir()).unwrap_or(false))
-        .map(|entry| entry.file_name())
-        .map(|name| name.to_string_lossy().into_owned());
-
-    let default_dir = match first_folder {
-        Some(folder) => scripts_dir.join(&folder),
-        None => {
-            println!("No folder found in scripts directory. Please enter the relative path to the desired folder:");
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).expect("Failed to read user input");
-            scripts_dir.join(input.trim())
+    let default_dir = loop {
+        println!("Please enter the relative path to the desired folder:");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read user input");
+        let folder_path = scripts_dir.join(input.trim());
+        if folder_path.is_dir() {
+            break folder_path;
+        } else {
+            println!("Invalid directory. Please try again.");
         }
     };
-
+    
     env::set_current_dir(&default_dir).expect("Failed to set default directory");
-
+    env::set_var("LD_LIBRARY_PATH", "./lib:${LD_LIBRARY_PATH}");
 
     let mut redis_pid: Option<u32> = None;
     let mut gunicorn_pid: Option<u32> = None;
 
-    env::set_var("LD_LIBRARY_PATH", "./lib:${LD_LIBRARY_PATH}");
-
     let mut rl = Editor::<()>::new();
     let server_op = Arc::new(Mutex::new(None));
 
+    // let mut redis_conf_path = REDIS_CONF_PATH.to_string();
+    // let mut workers = WORKERS.to_string();
+    // let mut address = ADDRESS.to_string();
+    // let mut timeout = TIMEOUT.to_string();
+    // let mut port = PORT.to_string();
+
+    let mut rl = Editor::<()>::new();
     loop {
         let readline = rl.readline(">> ");
         match readline {
@@ -64,7 +63,8 @@ fn main() {
 
                         *server_op_lock = Some(thread::spawn(move || {
                             // Replace this with the actual function to start the server
-                            if let Ok((redis_pid, gunicorn_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) {
+                            // REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT)
+                            if let Ok((redis_pid, gunicorn_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) {//&redis_conf_path, &workers, &address, &port, &timeout) {
                                 println!(
                                     "Server started with Redis PID: {}, Gunicorn PID: {}",
                                     redis_pid.unwrap_or(0),
@@ -77,26 +77,35 @@ fn main() {
                     },
                     "stop" => {
                         println!("Stopping server...");
-                        
-                        // Stop the server in a new thread
+
+                        // Capture the values of redis_pid and gunicorn_pid using the move keyword
+                        let redis_pid = redis_pid.expect("redis_pid not available");
+                        let gunicorn_pid = gunicorn_pid.expect("gunicorn_pid not available");
                         *server_op_lock = Some(thread::spawn(move || {
-                            // Replace this with the actual function to stop the server
-                            let redis_pid = redis_pid.expect("redis_pid not available");
-                            let gunicorn_pid = gunicorn_pid.expect("gunicorn_pid not available");
-                            server::stop_server(Some(redis_pid), Some(gunicorn_pid));
-                            println!("Server stopped.");
+                            if let (Some(redis_pid), Some(gunicorn_pid)) = (redis_pid, gunicorn_pid) {
+                                server::stop_server(Some(redis_pid), Some(gunicorn_pid));
+                                println!("Server stopped.");
+                            } else {
+                                println!("Server is not running.");
+                            }
                         }));
                     },
                     "restart" => {
                         println!("Restarting server...");
-                        
+                        let redis_pid = redis_pid.expect("redis_pid not available");
+                        let gunicorn_pid = gunicorn_pid.expect("gunicorn_pid not available");
                         // Restart the server in a new thread
                         *server_op_lock = Some(thread::spawn(move || {
-                            // Replace this with the actual function to restart the server
-                            let redis_pid = redis_pid.expect("redis_pid not available");
-                            let gunicorn_pid = gunicorn_pid.expect("gunicorn_pid not available");
-                            server::stop_server(Some(redis_pid), Some(gunicorn_pid));
-                            if let Ok((redis_pid, gunicorn_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) {
+                            if let (Some(redis_pid), Some(gunicorn_pid)) = (redis_pid, gunicorn_pid) {
+                                server::stop_server(Some(redis_pid), Some(gunicorn_pid));
+                                println!("Server stopped.");
+                            } else {
+                                println!("Server is not running.");
+                            }
+                            println!("Server stopped.");
+
+                            println!("Starting server...");
+                            if let Ok((redis_pid, gunicorn_pid)) = server::start_server(REDIS_CONF_PATH, WORKERS, ADDRESS, PORT, TIMEOUT) { // &redis_conf_path, &workers, &address, &port, &timeout) {
                                 println!(
                                     "Server started with Redis PID: {}, Gunicorn PID: {}",
                                     redis_pid.unwrap_or(0),
@@ -105,7 +114,6 @@ fn main() {
                             } else {
                                 println!("Error starting the server.");
                             }
-                            println!("Server restarted.");
                         }));
                     },
                     "update" => {
